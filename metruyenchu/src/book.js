8fc97f687ed04768a1971c9158e35f7f257ceb55
Hirooko <tranminh2000x@gmail.com>
2025-10-19 00:27:14 +0700

    -m -f extension_metruyenchu_v1.6

--- metruyenchu/src/book.js
+++ metruyenchu/src/book.js
@@ -2,31 +2,44 @@ load("config.js");
 
 function execute(url, page) {
     if (!page) page = '1';
-    let tocUrl = BASE_URL2.replace("https://", "https://backend.") + url + "&limit=20&page=" + page;
+    var tocUrl = "https://backend.metruyencv.com" + url + "&limit=20&page=" + page;
 
-    let response = Http.get(tocUrl, {
+    var response = fetch(tocUrl, {
         headers: {
-            "X-App": "MeTruyenChu"
-        }
+            "X-App": "MeTruyenChu",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 10; Mobile) Chrome/119.0.0.0 Safari/537.36"
+        },
     });
 
-    if (response.code === 200) {
-        let json = JSON.parse(response.text());
-        let novelList = [];
-        let next = json.pagination.next ? json.pagination.next + "" : null;
+    if (response.ok) {
+        var json = response.json();
+        var novelList = [];
+        var next = "";
+        if (json.pagination && json.pagination.next)
+            next = json.pagination.next + "";
 
-        json.data.forEach(book => {
+        for (var i = 0; i < json.data.length; i++) {
+            var book = json.data[i];
+            var authorName = "Không rõ tác giả";
+            if (book.author && book.author.name)
+                authorName = book.author.name;
+
+            var coverUrl = "";
+            if (book.poster && book.poster["default"])
+                coverUrl = book.poster["default"];
+
             novelList.push({
                 name: book.name,
                 link: book.link,
-                description: book.author?.name || "",
-                cover: book.poster?.["default"] || "",
+                description: authorName,
+                cover: coverUrl,
                 host: BASE_URL
             });
-        });
+        }
 
         return Response.success(novelList, next);
     }
 
+    console.log("Lỗi HTTP:", response.status);
     return null;
 }
--- metruyenchu/src/toc.js
+++ metruyenchu/src/toc.js
@@ -28,3 +28,13 @@ function execute(url) {
 
     return null;
 }
+
+
+load("book.js");
+
+function executeTest() {
+    const input = "/api/books?filter%5Bgender%5D=1&filter%5Bkind%5D=1&filter%5Bstate%5D=published&include=author%2Cgenres%2Ccreator&sort=-new_chap_at";
+    const result = execute(input, 1);
+    return result;
+}
+console.log(executeTest)
