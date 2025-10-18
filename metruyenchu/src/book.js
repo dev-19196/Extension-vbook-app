1852ab24438c87018f571d8aaa87904edab5dd40
Hirooko <tranminh2000x@gmail.com>
2025-10-18 23:55:20 +0700

    -m -f extension_metruyenchu_v1.5

--- metruyenchu/src/book.js
+++ metruyenchu/src/book.js
@@ -1,26 +1,30 @@
 load("config.js");
+
 function execute(url, page) {
     if (!page) page = '1';
     let tocUrl = BASE_URL2.replace("https://", "https://backend.") + url + "&limit=20&page=" + page;
 
-    let response = fetch(tocUrl, {
+    let response = Http.get(tocUrl, {
         headers: {
             "X-App": "MeTruyenChu"
-        },
+        }
     });
-    if (response.ok) {
-        let json = response.json();
+
+    if (response.code === 200) {
+        let json = JSON.parse(response.text());
         let novelList = [];
-        let next = json.pagination.next + "";
+        let next = json.pagination.next ? json.pagination.next + "" : null;
+
         json.data.forEach(book => {
             novelList.push({
                 name: book.name,
                 link: book.link,
-                description: book.author.name,
-                cover: book.poster['default'],
+                description: book.author?.name || "",
+                cover: book.poster?.["default"] || "",
                 host: BASE_URL
-            })
+            });
         });
+
         return Response.success(novelList, next);
     }
 
